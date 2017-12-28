382391c62a8cc9537b25fd5583d492ff3940cf88
Steven Senkowski <scsenkowski@gmail.com>
2017-12-28 18:33:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,10 +9,13 @@ end
 
 def self.create_table
   sql = <<- SQL
-
-  SQL
-
-  DB[:conn].execute(sql)
+  CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+          )
+      SQL
+      DB[:conn].execute(sql)
 end
 
 def self.drop_table
