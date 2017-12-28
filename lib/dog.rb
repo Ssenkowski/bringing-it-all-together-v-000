b3843495f576eadf78a484ffa603c8bfd43b1470
Steven Senkowski <scsenkowski@gmail.com>
2017-12-28 18:48:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,5 +74,21 @@ def self.find_or_create_by(name:, breed:)
    end
    dog
  end
+ def self.find_by_id(id)
+   sql = <<-SQL
+     SELECT *
+     FROM dogs
+     WHERE id = ?
+     LIMIT 1
+   SQL
 
+   DB[:conn].execute(sql,id).map do |row|
+     self.new_from_db(row)
+   end.first
+ end
+
+ def update
+   sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
+   DB[:conn].execute(sql, self.name, self.breed, self.id)
+ end
 end
