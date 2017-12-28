3dd31201b14021ec27beacd0650afe8e68592133
Steven Senkowski <scsenkowski@gmail.com>
2017-12-28 18:28:25 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+attr_accessor :name, :breed
+attr_reader :id
+
+def initialize(id = nil, name, breed)
+  @id, @name, @breed = id, name, breed
+end
+
+def self.create_table
+#  sql = <<- SQL
+#
+#  SQL
+#
+#  DB[:conn].execute(sql)
+end
+
+end
